7dd6ae74990c231d5555c29e62498faf927192b8
Santiago Cobas <aquaticsanti@gmail.com>
2026-03-01 20:47:24 -0300

    Made new entries show up in the main menu

--- people.py/__main__.py
+++ people.py/__main__.py
@@ -1,35 +1,19 @@
 # This follows the GeeksForGeeks tutorial here: https://www.geeksforgeeks.org/python/create-first-gui-application-using-python-tkinter/
 from tkinter import *
+from tkinter import ttk
+import time
 import sqlite3
 
-db = sqlite3.connect('people.db')
-cur = db.cursor()
-if cur.execute("SELECT name FROM sqlite_master").fetchone() == None:
-    empty = True
-    id = 0
-    cur.execute("""CREATE TABLE people(
-    id            INTEGER PRIMARY KEY AUTOINCREMENT,
-    name          text,
-    surname       text,
-    phone         text,
-    email         text);""")
-else:
-    empty = False
-    id = cur.execute("select id from people order by rowid desc LIMIT 1")
-    if id.fetchone() == None:
-        empty = True
-        id = 0
-    else:
-        print(id.fetchone())
-        id = id.fetchone()
-
 def NewContact():
+    root.destroy()
     def saveExit():
         contactInfo = []
         for entry in dynamic_entry:
             contactInfo.append(str(entry.get()))
         cur.execute(f"INSERT INTO people (name, surname, phone, email) VALUES (?, ?, ?, ?)", (contactInfo))
         db.commit()
+        global root
+        root = main()
         newContactWindow.destroy()
     newContactWindow = Tk()
     newContactWindow.title("Create new Contact")
@@ -55,11 +39,28 @@ def NewContact():
     newContactWindow.mainloop()
 
 def main() -> Tk:
+    global db
+    db = sqlite3.connect('people.db')
+    global cur
+    cur = db.cursor()
+    if cur.execute("SELECT name FROM sqlite_master").fetchone() == None:
+        empty = True
+        cur.execute("""CREATE TABLE people(
+        id            INTEGER PRIMARY KEY AUTOINCREMENT,
+        name          text,
+        surname       text,
+        phone         text,
+        email         text);""")
+    else:
+        empty = False
+        id = cur.execute("select id from people order by rowid desc LIMIT 1")
+        if id.fetchone() == None:
+            empty = True
     root = Tk()
     # root window title and dimension
     root.title("people.py")
     # Set geometry(widthxheight)
-    root.geometry('350x200')
+    root.geometry('375x200')
     # adding menu bar in root window
     # new item in menu bar labelled as 'New'
     # adding more items in the menu bar 
@@ -72,21 +73,32 @@ def main() -> Tk:
         # adding a label to the root window
         lbl = Label(root, text = "Create some contacts to get started!")
         lbl.grid()
-    # adding Entry Field
-    #  txt = Entry(root, width=10)
-    #  txt.grid(column =1, row =0)
-    # function to display user text when
-    # button is clicked
-    #  def clicked():
-    #      res = "You wrote" + txt.get()
-    #      lbl.configure(text = res)
-    # button widget with red color text inside
-    #  btn = Button(root, text = "Click me" ,
-    #              fg = "red", command=clicked)
-    # Set Button Grid
-    #  btn.grid(column=2, row=0)
+    else:
+        rows = cur.execute("SELECT * FROM people")
+        rows = rows.fetchall()
+        rows.insert(0, ('ID', 'Name', 'Surname', 'Phone', 'Email'))
+        i = 0
+        for j in rows:
+            h = 0
+            for k in j:
+                lbl = Label(root, text=k, font=("TkDefaultFont", 12), wraplength=0.25)
+                lbl.grid(column=h, row=i)
+                h += 1
+            if i > 0:
+                ttk.Separator(root, orient="horizontal").grid(row=i-1, sticky="ew", columnspan=10, padx=0, pady=0)
+                ttk.Separator(root, orient="horizontal").grid(row=i+1, sticky="ew", columnspan=10, padx=0, pady=0)
+            else:
+                pass
+            
+            i += 2
     btn2 = Button(root, text="Exit", command=root.destroy)
-    btn2.grid(column=3, row=0)
+    try:
+        if i == 0:
+            pass
+    except UnboundLocalError:
+        btn2.grid(column=0, row=1)
+    else:
+        btn2.grid(column=0, row=i)
     return root
 # Execute Tkinter
 root = main()
